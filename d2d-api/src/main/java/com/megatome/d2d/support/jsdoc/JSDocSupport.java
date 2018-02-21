bfa9405a4f69210a4be7e1cbf3b610c7887e3ccc
Gerry Weißbach <gweissbach@inetsoftware.de>
2018-02-21 07:52:13 +0100

    Update usage of JSON API

--- d2d-api/src/main/java/com/megatome/d2d/support/jsdoc/JSDocSupport.java
+++ d2d-api/src/main/java/com/megatome/d2d/support/jsdoc/JSDocSupport.java
@@ -6,8 +6,8 @@ import static org.apache.commons.io.FileUtils.readFileToString;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.megatome.d2d.exception.BuilderException;
 import com.megatome.d2d.support.DocSetParserInterface;
@@ -22,56 +22,58 @@ public class JSDocSupport implements DocSetParserInterface {
     public static final String JSDOC_IMPLEMENTATION = "jsdoc";
 
     @Override
-    public IndexData findIndexFile(File javadocDir) throws BuilderException {
+    public IndexData findIndexFile( File javadocDir ) throws BuilderException {
         final IndexData indexData = new IndexData();
 
-        if (!javadocDir.exists() || !javadocDir.isDirectory()) {
-            throw new BuilderException(String.format("%s does not exist, or is not a directory", javadocDir.getAbsolutePath()));
+        if( !javadocDir.exists() || !javadocDir.isDirectory() ) {
+            throw new BuilderException( String.format( "%s does not exist, or is not a directory", javadocDir.getAbsolutePath() ) );
         }
 
-        logVerbose("Looking for index.json file");
+        logVerbose( "Looking for index.json file" );
 
-        File indexJson = new File(javadocDir, "index.json");
-        if ( indexJson.exists() ) {
-            indexData.addFileToIndex( new File(javadocDir, "index.json"));
+        File indexJson = new File( javadocDir, "index.json" );
+        if( indexJson.exists() ) {
+            indexData.addFileToIndex( new File( javadocDir, "index.json" ) );
         }
 
-        if ( new File(javadocDir, "index.html").exists() ) {
+        if( new File( javadocDir, "index.html" ).exists() ) {
             indexData.setDocsetIndexFile( "index.html" );
         }
 
-        if (!indexData.hasFilesToIndex()) {
-            throw new BuilderException(String.format("Did not find any jsdoc files. Make sure that %s is a directory containing jsodc", javadocDir.getAbsolutePath()));
+        if( !indexData.hasFilesToIndex() ) {
+            throw new BuilderException( String.format( "Did not find any jsdoc files. Make sure that %s is a directory containing jsodc", javadocDir.getAbsolutePath() ) );
         }
 
-        logVerbose("Found jsdoc files");
+        logVerbose( "Found jsdoc files" );
 
         return indexData;
     }
 
     @Override
-    public List<SearchIndexValue> findSearchIndexValues(List<File> filesToIndex) throws BuilderException {
+    public List<SearchIndexValue> findSearchIndexValues( List<File> filesToIndex ) throws BuilderException {
         final List<SearchIndexValue> values = new ArrayList<>();
-        for (final File f : filesToIndex) {
-            final List<SearchIndexValue> indexValues = indexFile(f);
-            values.addAll(indexValues);
+        for( final File f : filesToIndex ) {
+            final List<SearchIndexValue> indexValues = indexFile( f );
+            values.addAll( indexValues );
         }
         return values;
     }
 
-    private static List<SearchIndexValue> indexFile(File f) throws BuilderException {
+    @SuppressWarnings( "unchecked" )
+    private static List<SearchIndexValue> indexFile( File f ) throws BuilderException {
         final List<SearchIndexValue> values = new ArrayList<>();
 
         try {
-            @SuppressWarnings( "unchecked" )
-            HashMap<String, ArrayList<HashMap<String, String>>> jsonObject= (HashMap<String, ArrayList<HashMap<String, String>>>)new JsonSlurper().parseText( readFileToString(f) );
+            Map<String, Object> jsonObject = (Map<String, Object>)new JsonSlurper().parseText( readFileToString( f ) );
 
-            jsonObject.forEach( (String module, ArrayList<HashMap<String, String>> entries) -> {
+            jsonObject.forEach( ( String module, Object entries ) -> {
                 MatchTypeInterface matchType = JSDocMatchType.type( module );
-                entries.forEach( (HashMap<String, String> property) -> {
-                    values.add(new SearchIndexValue(property.get( "name" ), matchType, link(property.get( "link" ))));
-                });
-            });
+                if ( entries instanceof ArrayList ) {
+                    ((ArrayList<Map<String, Object>>)entries).forEach( ( Map<String, Object> property ) -> {
+                        values.add( new SearchIndexValue( (String)property.get( "name" ), matchType, link( (String)property.get( "link" ) ) ) );
+                    } );
+                }
+            } );
         } catch( IOException e ) {
             throw new BuilderException( e.getLocalizedMessage() );
         }
@@ -79,7 +81,7 @@ public class JSDocSupport implements DocSetParserInterface {
         return values;
     }
 
-    private static String link(String link) {
+    private static String link( String link ) {
         return link;
     }
 }
